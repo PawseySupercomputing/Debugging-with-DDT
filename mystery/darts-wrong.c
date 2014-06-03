03a3b0a717171752be4cbedcf501b406ceb6fb64
Daniel Grimwood <reaper@ivec.org>
2014-06-03 16:26:39 +0800

    Initial version.

---
+++ mystery/darts-wrong.c
@@ -0,0 +1,48 @@
+#include <omp.h>
+#include "lcgenerator.h"
+#include <mpi.h>
+
+// This code gets the wrong answer.
+
+static long num_trials = 1000000;
+
+int main(int argc, char **argv) {
+  long i;
+  long Ncirc = 0;
+  double pi, x, y;
+  double r = 1.0; // radius of circle
+  double r2 = r*r;
+
+  int rank, size, manager = 0;
+  MPI_Status status;
+  long my_trials, temp;
+  int provided;
+
+  MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  my_trials = num_trials/size;
+  if (num_trials%(long)size > (long)rank) my_trials++;
+  random_last = rank;
+
+#pragma omp parallel
+{
+#pragma omp for private(x,y) reduction(+:Ncirc)
+  for (i = 0; i < num_trials; i++) {
+    x = lcgrandom();
+    y = lcgrandom();
+    if ((x*x + y*y) <= r2)
+      Ncirc++;
+  }
+}
+
+  MPI_Reduce(&Ncirc, &temp, 1, MPI_LONG, MPI_SUM, manager, MPI_COMM_WORLD);
+
+  if (rank == manager) {
+    Ncirc = temp;
+    pi = 4.0 * ((double)Ncirc)/((double)num_trials);
+    printf("\n For %ld trials, pi = %f\n", num_trials, pi);
+  } 
+  MPI_Finalize();
+  return 0;
+}
